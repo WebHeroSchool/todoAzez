8fbbeb21322c634401be2b971fbbdcee84c42b2f
Александр Чернилевский <aleksanderterar@gmail.com>
2021-02-14 23:36:45 +0300

    applic2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from'react';
 
-const element = (<div>О_О
+const ItemsList = () => (<ul>
+	<li>Первый Пункт Делишек</li>
+	<li>Второй Пункт Делишек</li>
+	<li>Третий Пункт Делишек</li>
+	<li>Четвёртый Пункт Делишек</li>
+	<li>Пятый Пункт Делишек</li>
+</ul>);
+
+  const App = () => (<div><h1>О_О</h1>
   <button>Жмякай</button>
   <p>А то укушу</p>
-  </div>);
+  <ItemsList />
+ </div>);
 
-export {element};
+export default App;
