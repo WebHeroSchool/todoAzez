d3d0f1e55c8407cd6a0ea49030c10820375e7a18
Александр Чернилевский <aleksanderterar@gmail.com>
2021-02-17 21:31:43 +0300

    material-ui

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItems = () => (<div>InputItem</div>);
+const InputItems = () => (<div>
+	<TextField
+	    id="standard-number"
+	    label="Напиши что нибудь для меня"
+	    InputLabelProps={{
+	    shrink: true,
+	    }}
+    />
+</div>);
 
 export default InputItems;
