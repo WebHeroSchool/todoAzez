a0ab08925161277b3d201b96fda0e3ec9dd6fb8b
Александр Чернилевский <aleksanderterar@gmail.com>
2021-02-21 23:35:47 +0300

    onClickDone

--- src/components/Item/items.js
+++ src/components/Item/items.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import classnames from 'classnames';
-import styles from '../Item/item.module.css'
+import styles from '../Item/item.module.css';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteForeverRoundedIcon from '@material-ui/icons/DeleteForeverRounded';
 
-const Item = ({ value, isDone }) => (<span className={
-	classnames({
-		[styles.item]: true,
-		[styles.done]: isDone
-	})
-}>
-	{value}
-</span>);
+const Item = ({ value, isDone, onClickDone }) => (
+	<ListItem className={
+		classnames({
+			[styles.item]: true,
+			[styles.done]: isDone
+		})
+	}>
+		<Checkbox
+			onClick={() => onClickDone(isDone)} 
+		/>
+		<ListItemText> {value}</ListItemText>
+    <ListItemSecondaryAction className={styles.delete}>
+      <IconButton aria-label="Comments">
+        <DeleteForeverRoundedIcon />
+      </IconButton>
+    </ListItemSecondaryAction>
+  </ListItem>
+  );
 
 export default Item;
