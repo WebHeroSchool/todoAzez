5acc7ada7c7656787f8775426c0559f74b8b1d71
Александр Чернилевский <aleksanderterar@gmail.com>
2021-04-30 00:45:50 +0300

    trash

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,46 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import styles from './App.module.css';
 import Todo from '../Todo/Todo.js';
-import Contacts from '../Contacts/Contacts.js';
 import About from '../About/About.js';
-import Card from '@material-ui/core/Card';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
+// import Contacts from '../Contacts/Contacts.js';
+// import Card from '@material-ui/core/Card';
+// import MenuItem from '@material-ui/core/MenuItem';
+// import MenuList from '@material-ui/core/MenuList';
 
 
 const App = () => 
 (<Router>
-	<div className={styles.wrap}>
-		<Card className={styles.sidebar}>
-			<MenuList>
-				<Link to='/'  className={styles.link}><MenuItem className={styles.item}>Про меня</MenuItem></Link>
-				<Link to='/todo'  className={styles.link}><MenuItem className={styles.item}>Дела</MenuItem></Link>
-				<Link to='/contacts'  className={styles.link}><MenuItem className={styles.item}>Контакты</MenuItem></Link>
-			</MenuList>
-		</Card>
-
-		<Card className={styles.content}>
-			<Route path='/' exact component={About} />
-			<Route path='/todo' component={Todo} />
-			<Route path='/contacts' component={Contacts} />
-		</Card>
-	</div>
+  <div className={styles.wrap}>
+    <header className={styles.header}>
+      <a 
+        href='https://webheroschool.ru/'
+        target='_blank'
+        rel='noopener noreferrer'
+        className={styles.school_link}
+      >
+        <div className={styles.school_logo}></div>
+      </a>
+      <NavLink 
+        to='/'
+        exact
+        className={styles.header__link}
+        activeClassName={styles.header__link_active}>
+          Про меня
+      </NavLink>
+      <NavLink 
+        to='/todo'
+        exact
+        className={styles.header__link} 
+        activeClassName={styles.header__link_active}>
+          Дела
+      </NavLink>
+    </header>
+    <div className={styles.content}>
+      <Route path='/' exact component={About} />
+      <Route path='/todo' component={Todo} />
+    </div>
+  </div>
 </Router>);
 
 export default App;
